44cbecb9dbe872f79e3db87adab1332e22ca1061
Cowboy <vn649271@gmail.com>
2022-05-28 08:22:15 +0200

    - Implemented "address copied" toast box in WalletAddressBox

--- src/block-frontend/src/components/WalletAddressBox.js
+++ src/block-frontend/src/components/WalletAddressBox.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { connect } from 'react-redux';
+import Modal from "react-bootstrap/Modal";
 
 const WalletAddressBox = props => {
 
@@ -10,6 +11,7 @@ const WalletAddressBox = props => {
 
 	const [_wallet_address, setWalletAddress] = useState(null);
 	const [_user_name, setUserName] = useState(null);
+	const [showCopiedMessage, setShowCopiedMessage] = useState(false);
 
 	useEffect(() => {
 		if (userName !== undefined && userName !== null && userName !== "") {
@@ -21,6 +23,8 @@ const WalletAddressBox = props => {
 		if (navigator.clipboard) {
 			navigator.clipboard.writeText(_wallet_address).then(function() {
 				console.log('Async: Copying to clipboard was successful!');
+				setShowCopiedMessage(true);
+				setTimeout(hideCopiedMessage, 700);
 			}, function(err) {
 				console.error('Async: Failed to copy text into clipboard: ', err);
 			});			
@@ -29,23 +33,37 @@ const WalletAddressBox = props => {
 		}
 		console.log(history.location);
 	}
+	const hideCopiedMessage = () => setShowCopiedMessage(false);
 
 	return  (
-		<div className="w-full">
-		{
-			_wallet_address && history.location.pathname !== "/" ? 
-			<div>
-				<div className="wallet-address" onClick={onClick} tooltip="Copy to clipboard">
+		<>
+			<div className="text-right">
+				<div className="wallet-address-box w-full">
 				{
-					_user_name + ": " + _wallet_address.substring(0, 6) + 
-					"..." + 
-					_wallet_address.substring(_wallet_address.length-5, _wallet_address.length-1)
+					_wallet_address && history.location.pathname !== "/" ? 
+					<div>
+						<div className="wallet-address" onClick={onClick} tooltip="Copy to clipboard">
+						{
+							_user_name + ": " + _wallet_address.substring(0, 6) + 
+							"..." + 
+							_wallet_address.substring(_wallet_address.length-5, _wallet_address.length-1)
+						}
+						</div>
+						{/******************* "Minting..." Dialog *********************/}
+						<div className={`${showCopiedMessage?"show-toast-box":"hide-toast-box"}`} onClick={hideCopiedMessage}>
+							<div className="check-icon flow-layout">
+						    	<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+								  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+								</svg>
+							</div>
+							<div className="flow-layout">Copied!</div>
+						</div>
+					</div>:
+					<></>
 				}
-				</div>
-			</div>:
-			<></>
-		}
-        </div>
+		        </div>
+	        </div>
+        </>
 	)
 }
 
