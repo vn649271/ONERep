55aa4fc186cccdacdf7ccdf38340aebebc8384e6
Cowboy <vn649271@gmail.com>
2022-05-30 09:31:35 +0200

    + Changed backend address to ngrok router

--- src/block-frontend/src/conf.js
+++ src/block-frontend/src/conf.js
@@ -1,4 +1,4 @@
 const is_test = false;
 module.exports = {
-  SERVER_URL: is_test ? "http://localhost:3001" : "http://da57-116-202-24-219.ngrok.io",
+  SERVER_URL: is_test ? "http://localhost:3001" : "http://4555-116-202-24-219.ngrok.io",
 };
