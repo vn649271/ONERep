a2b5d2feb59e3ba5d2d7ea97b70dd8b1ec6eb918
Cowboy <vn649271@gmail.com>
2022-06-09 15:26:42 +0200

    +

--- src/block-frontend/src/conf.js
+++ src/block-frontend/src/conf.js
@@ -1,4 +1,4 @@
 const is_test = false;
 module.exports = {
-  SERVER_URL: is_test ? "http://localhost:3001" : "https://onerep-api1.herokuapp.com/",
+  SERVER_URL: is_test ? "http://localhost:3001" : "https://onerep-api1.herokuapp.com",
 };
