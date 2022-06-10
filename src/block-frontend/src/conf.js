32dd7d53846b6b701494f2709f31e1528bb76945
Cowboy <vn649271@gmail.com>
2022-06-11 00:33:42 +0200

    http -->> https

--- src/block-frontend/src/conf.js
+++ src/block-frontend/src/conf.js
@@ -1,4 +1,4 @@
 const is_test  = false;
 module.exports = {
-  SERVER_URL: is_test ? "http://localhost:3001" : "http://116.202.24.219:80",
+  SERVER_URL: is_test ? "http://localhost:3001" : "https://116.202.24.219:80",
 };
