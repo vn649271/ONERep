38936cd42e373760892b5e7d5df54109886729cc
Ubuntu <ubuntu@ip-172-26-9-21.ap-southeast-1.compute.internal>
2022-06-10 22:33:46 +0000

    Updates

---
+++ precache-manifest.cd1ab870ee44ad016cb04ab50e032643.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f8c50813d7bc0e6770ccf5dddd4a92",
+    "url": "/onerep-frontend/index.html"
+  },
+  {
+    "revision": "04177942e02714a7e482",
+    "url": "/onerep-frontend/static/css/2.bc6bc193.chunk.css"
+  },
+  {
+    "revision": "eef05db1e28351d45844",
+    "url": "/onerep-frontend/static/css/main.2190f434.chunk.css"
+  },
+  {
+    "revision": "04177942e02714a7e482",
+    "url": "/onerep-frontend/static/js/2.88374c24.chunk.js"
+  },
+  {
+    "revision": "145508895de5704e2fa41334a350af53",
+    "url": "/onerep-frontend/static/js/2.88374c24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef05db1e28351d45844",
+    "url": "/onerep-frontend/static/js/main.661100bb.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/onerep-frontend/static/js/main.661100bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a46585ebdce74b17f60",
+    "url": "/onerep-frontend/static/js/runtime-main.9f293b92.js"
+  }
+]);
