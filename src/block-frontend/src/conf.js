fd1815139a849d03c8c110a3babd71ba48a9043c
Cowboy <vn649271@gmail.com>
2022-06-11 23:32:32 +0200

    +

--- src/block-frontend/src/conf.js
+++ src/block-frontend/src/conf.js
@@ -1,8 +1,8 @@
 const deploy_mode = 2; // 0: localhost, 1: publish on host, 2: publish on dedicated server
 module.exports = {
 	SERVER_URL: deploy_mode === 0 ? 
-					"http://localhost:3001" : 
-					deploy_mode === 1 ? 
+					"http://localhost:3001" :
+					( deploy_mode === 1 ? 
 						"https://2b16-116-202-24-219.ngrok.io":
-						"https://onerep.uniblocks.net:3001"
+						"https://onerep.uniblocks.net:3001" )
 };
