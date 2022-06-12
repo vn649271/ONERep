8e9157fefbe3ee42307d68b5fb87ab15a600ea1b
vn649271 <vn649271@gmail.com>
2022-06-12 12:23:18 +0200

    <->

--- src/block-frontend/src/conf.js
+++ src/block-frontend/src/conf.js
@@ -1,8 +1,8 @@
-const deploy_mode = 2; // 0: localhost, 1: publish on host, 2: publish on dedicated server
+const deploy_mode = 1; // 0: localhost, 1: publish on host, 2: publish on dedicated server
 module.exports = {
 	SERVER_URL: deploy_mode === 0 ? 
 					"http://localhost:3001" :
 					( deploy_mode === 1 ? 
-						"https://2b16-116-202-24-219.ngrok.io":
+						"https://4557-116-202-24-219.ngrok.io":
 						"https://172.26.9.21:3001" )
 };
